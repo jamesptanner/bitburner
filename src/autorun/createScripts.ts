bb41e214c6a41e1d114e7e0da4ad6a9117e39544
James Tanner <admin@codingentity.co.uk>
2022-02-16 21:05:54 +0000

    Checks to see if we could create a script.

--- src/autorun/createScripts.ts
+++ src/autorun/createScripts.ts
@@ -1,5 +1,30 @@
 import { NS } from '@ns'
 
-export async function main(ns : NS) : Promise<void> {
-    // nothing.
+const scripts = new Map<string, number>([
+    ["AutoLink.exe", 25],
+    ["BruteSSH.exe", 50],
+    ["ServerProfiler.exe", 75],
+    ["DeepscanV1.exe", 75],
+    ["FTPCrack.exe", 100],
+    ["relaySMTP.exe", 250],
+    ["DeepscanV2.exe", 400],
+    ["HTTPWorm.exe", 500],
+    ["SQLInject.exe", 750],
+]);
+export async function main(ns: NS): Promise<void> {
+    const player = ns.getPlayer();
+    while (true) {
+        ns.tprintf(`INFO: Checking if we can make any scripts`);
+        for (const iterator of scripts) {
+            const script = iterator[0];
+            const cost = iterator[1];
+            if (!ns.fileExists(script) && player.hacking >= cost) {
+                if (!ns.isBusy()) {
+                    ns.tprintf(`INFO: starting work on new script: ${script}`);
+                    //ns.createProgram(script);
+                }
+            }
+        }
+        await ns.sleep(5 * 60 * 1000);
+    }
 }
