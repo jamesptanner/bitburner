a676300d970669ca1c32df259a191283b7ae14f4
James Tanner <admin@codingentity.co.uk>
2022-02-17 23:14:21 +0000

    Begin hacking the host if the backdoor is installed. Record hosts i need to backdoor manually for the moment.

--- src/walker.ts
+++ src/walker.ts
@@ -7,6 +7,7 @@ export async function main(ns: NS): Promise<void> {
     const alreadyScanned = [];
     if (typeof parent === "string") {
         const hosts = ns.scan(parent);
+        const toBackdoor = []
         while (hosts.length > 0){
             const currentHost = hosts.pop();
             if(alreadyScanned.indexOf(currentHost) != -1){
@@ -19,10 +20,21 @@ export async function main(ns: NS): Promise<void> {
                 ns.tprintf(`💣 ${currentHost}`);
 
                 ns.exec("infiltrate.js", "home",1,currentHost);
+                toBackdoor.push(currentHost)
+            }
+            else if(serverInfo.backdoorInstalled && currentHost ){
+                await ns.scp(["HGW.js","hackHost.js"],currentHost);
+                const memReq = ns.getScriptRam("hackHost.js");
+                const avalibleRam = serverInfo.maxRam -serverInfo.ramUsed;
+                ns.tprintf(`Mem: avalible:${avalibleRam}, total:${serverInfo.maxRam}, needed:${memReq}`)
+                if(ns.exec("hackHost.js",currentHost,Math.max(1,Math.floor(avalibleRam/memReq)-1),currentHost)==0){
+                    ns.tprintf(`failed to launch script on ${currentHost}`)
+                }
             }
             alreadyScanned.push(currentHost);
             await ns.sleep(1000);
         }
+        await ns.write("toBackdoor",toBackdoor,"w");
     }
 
 }
