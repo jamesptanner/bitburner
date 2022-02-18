eb5aac42261172c2147aa47b0db2fbbc9cf2befd
James Tanner <admin@codingentity.co.uk>
2022-02-18 08:16:14 +0000

    make the killscript walk.

--- src/killscript.ts
+++ src/killscript.ts
@@ -1,12 +1,17 @@
 import { NS } from '@ns'
+import { walk } from './utils';
 
 export async function main(ns : NS) : Promise<void> {
     const target = ns.args[0] || "";
     ns.tprintf(`INFO killing script: ${target}`)
     if (typeof target === 'string') {
-    }
+    
+        await walk(ns,"home",async (ns,host,target): Promise<boolean> => {
+            ns.ps(host).filter(x => target ==="" ||x.filename ===target).forEach(x =>{
+            ns.kill(x.pid)});
 
-    ns.ps().filter(x => target ==="" ||x.filename ===target).forEach(x =>{
-ns.kill(x.pid);
-    });
+            return true;
+        },target)
+
+    }
 }
