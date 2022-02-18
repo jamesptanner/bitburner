cc477600e82caf3625d086251a83bd42521f0350
James Tanner <jamesptanner@outlook.com>
2022-02-18 07:51:33 +0000

    extract the node walking out to its own function.

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,24 @@
+import { NS } from '@ns';
+
 export function asString(val: any): string | null{
     if (typeof val === "string") return val;
     return null;
+}
+
+export async function walk(ns: NS, start: string, func: (ns: NS, host: string | undefined, ...args: any[]) => Promise<boolean>, ...args: any[]): Promise<void> {
+    const alreadyScanned = [];
+    const hosts = ns.scan(start);
+    while (hosts.length > 0) {
+        const currentHost = hosts.pop();
+        if (alreadyScanned.indexOf(currentHost) != -1) {
+            continue;
+        }
+        ns.tprintf(`Scanning ${currentHost}`);
+        hosts.push(...ns.scan(currentHost));
+        const cont = await func(ns, currentHost, ...args);
+        if (!cont)
+            break;
+        alreadyScanned.push(currentHost);
+        await ns.sleep(1000);
+    }
 }
--- src/walker.ts
+++ src/walker.ts
@@ -1,47 +1,40 @@
 import { NS } from '@ns'
-import { parentPort } from 'worker_threads';
+import { walk } from './utils';
 
 export async function main(ns: NS): Promise<void> {
     //
     const parent = ns.args?.[0] || "home";
-    const alreadyScanned = [];
     if (typeof parent === "string") {
-        const hosts = ns.scan(parent);
-        const toBackdoor = []
-        while (hosts.length > 0){
-            const currentHost = hosts.pop();
-            if(alreadyScanned.indexOf(currentHost) != -1){
-                continue;
-            }
-            ns.tprintf(`Scanning ${currentHost}`);
-            hosts.push(...ns.scan(currentHost));
-            const serverInfo = ns.getServer(currentHost)
-            if(!serverInfo.backdoorInstalled && currentHost && currentHost){
-                ns.tprintf(`💣 ${currentHost}`);
-                const targethacklevel = ns.getServerRequiredHackingLevel(currentHost)
-                if (targethacklevel > ns.getHackingLevel()) {
-                    ns.tprintf(`INFO not able to hack host: ${currentHost}(${targethacklevel})`)
-                }
-                else {
-                    if(!serverInfo.purchasedByPlayer){
-                        ns.exec("infiltrate.js", "home",1,currentHost);
-                        toBackdoor.push(currentHost)
-                    }
-                }
-            }
-            else if(serverInfo.backdoorInstalled && currentHost ){
-                await ns.scp(["HGW.js","hackHost.js"],currentHost);
-                const memReq = ns.getScriptRam("hackHost.js");
-                const avalibleRam = serverInfo.maxRam -serverInfo.ramUsed;
-                ns.tprintf(`Mem: avalible:${avalibleRam}, total:${serverInfo.maxRam}, needed:${memReq} threads=${Math.max(1,Math.floor(avalibleRam/memReq)-1)}`)
-                if(ns.exec("hackHost.js",currentHost,Math.max(1,Math.floor(avalibleRam/memReq)-1),currentHost)==0){
-                    ns.tprintf(`failed to launch script on ${currentHost}`)
-                }
-            }
-            alreadyScanned.push(currentHost);
-            await ns.sleep(1000);
-        }
+        const toBackdoor: string[] = []
+        await walk(ns,parent,backdoor,toBackdoor);
         await ns.write("toBackdoor.txt",JSON.stringify(toBackdoor),"w");
     }
 
+}
+
+async function backdoor(ns: NS, currentHost: string | undefined, toBackdoor: string[]) :Promise<boolean> {
+    const serverInfo = ns.getServer(currentHost);
+    if (!serverInfo.backdoorInstalled && currentHost && currentHost) {
+        ns.tprintf(`💣 ${currentHost}`);
+        const targethacklevel = ns.getServerRequiredHackingLevel(currentHost);
+        if (targethacklevel > ns.getHackingLevel()) {
+            ns.tprintf(`INFO not able to hack host: ${currentHost}(${targethacklevel})`);
+        }
+        else {
+            if (!serverInfo.purchasedByPlayer) {
+                ns.exec("infiltrate.js", "home", 1, currentHost);
+                toBackdoor.push(currentHost);
+            }
+        }
+    }
+    else if (serverInfo.backdoorInstalled && currentHost) {
+        await ns.scp(["HGW.js", "hackHost.js"], currentHost);
+        const memReq = ns.getScriptRam("hackHost.js");
+        const avalibleRam = serverInfo.maxRam - serverInfo.ramUsed;
+        ns.tprintf(`Mem: avalible:${avalibleRam}, total:${serverInfo.maxRam}, needed:${memReq} threads=${Math.max(1, Math.floor(avalibleRam / memReq) - 1)}`);
+        if (ns.exec("hackHost.js", currentHost, Math.max(1, Math.floor(avalibleRam / memReq) - 1), currentHost) == 0) {
+            ns.tprintf(`failed to launch script on ${currentHost}`);
+        }
+    }
+    return true;
 }
