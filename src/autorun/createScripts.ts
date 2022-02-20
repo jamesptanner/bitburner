8d03dc1c52f96292a54658b80915b08e42d978d8
James Tanner <jamesptanner@outlook.com>
2022-02-20 23:06:17 +0000

    no need for inf loop.

--- src/autorun/createScripts.ts
+++ src/autorun/createScripts.ts
@@ -4,13 +4,11 @@ import { scripts } from '/utils/HGW';
 
 export async function main(ns: NS): Promise<void> {
     const player = ns.getPlayer();
-    while (true) {
-        for (const iterator of scripts) {
-            const script = iterator[0];
-            const cost = iterator[1];
-            if (!ns.fileExists(script) && player.hacking >= cost) {
-                ns.tprintf(`INFO: You should work on new script: ${script}`);
-            }
+    for (const iterator of scripts) {
+        const script = iterator[0];
+        const cost = iterator[1];
+        if (!ns.fileExists(script) && player.hacking >= cost) {
+            ns.tprintf(`INFO: You should work on new script: ${script}`);
         }
     }
 }
