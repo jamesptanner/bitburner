4da1c9e0c96c1e1344a52695743c31ac12aa50bd
James Tanner <jamesptanner@outlook.com>
2022-02-20 18:00:21 +0000

    minTriangle path (untested)

--- src/contracts/solvers/PathContracts.ts
+++ src/contracts/solvers/PathContracts.ts
@@ -6,8 +6,26 @@ import { unimplemented } from '/contracts/contractUtils';
 // the one before it, forming a triangle). Find the minimum path sum from the top to the
 // bottom of the triangle. In each step of the path, you may only move to adjacent
 // numbers in the row below.
-export function MinTrianglePath(ns:NS,data:any):number|string[]|undefined{return unimplemented(data)}
+export function MinTrianglePath(ns: NS, data: any): number | string[] | undefined {
+    ns.tprintf(`${JSON.stringify(data)} type:${typeof data}`);
+    const numberArray = data;
 
+    for (let row = 1; row < numberArray.length; row++) {
+        for (let col = 1; col < numberArray[row].length; col++) {
+
+            if (col === 0) {
+                numberArray[row][col] += numberArray[row - 1][col]
+            }
+            else if (col === numberArray[row].length - 1) {
+                numberArray[row][col] += numberArray[row - 1][col - 1]
+            }
+            else {
+                numberArray[row][col] += Math.min(numberArray[row - 1][col], numberArray[row - 1][col - 1])
+            }
+        }
+    }
+    return Math.min(...numberArray[numberArray.length - 1]);
+}
 
 // "Unique Paths in a Grid I"
 
@@ -17,7 +35,7 @@ export function MinTrianglePath(ns:NS,data:any):number|string[]|undefined{return
 // you may only move down or to the right.
 
 // Determine how many unique paths there are from start to finish.
-export function UniquePath1(ns:NS,data:any):number|string[]|undefined{
+export function UniquePath1(ns: NS, data: any): number | string[] | undefined {
     ns.tprintf(`${JSON.stringify(data)} type:${typeof data}`)
     const maxX: number = data[0]
     const maxY: number = data[1]
@@ -25,16 +43,16 @@ export function UniquePath1(ns:NS,data:any):number|string[]|undefined{
     const map: number[][] = []
 
     for (let x = 0; x < maxX; x++) {
-        map[x]=[]
+        map[x] = []
         for (let y = 0; y < maxY; y++) {
-            if(x==0 || y==0){
+            if (x == 0 || y == 0) {
                 map[x][y] = 1
             }
-            map[x][y]= map[x-1][y] + map[x][y-1];
+            map[x][y] = map[x - 1][y] + map[x][y - 1];
         }
     }
-    ns.tprintf(`paths: ${map[maxX-1][maxY-1]}`)
-    return map[maxX-1][maxY-1]
+    ns.tprintf(`paths: ${map[maxX - 1][maxY - 1]}`)
+    return map[maxX - 1][maxY - 1]
 }
 
 
@@ -49,4 +67,4 @@ export function UniquePath1(ns:NS,data:any):number|string[]|undefined{
 // or to the right. Furthermore, you cannot move onto spaces which have obstacles.
 
 // Determine how many unique paths there are from start to finish.
-export function UniquePath2(ns:NS,data:any):number|string[]|undefined{return unimplemented(data)}
+export function UniquePath2(ns: NS, data: any): number | string[] | undefined { return unimplemented(data) }
