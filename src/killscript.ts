2998ade8cef5cb2716ea44df7a2b37a552bd427d
James Tanner <jamesptanner@outlook.com>
2022-02-22 21:38:44 +0000

    refactor out use of walk()

--- src/killscript.ts
+++ src/killscript.ts
@@ -1,17 +1,15 @@
 import { NS } from '@ns'
-import { walk } from '/utils/utils';
+import { getAllServers } from '/utils/utils';
 
 export async function main(ns : NS) : Promise<void> {
     const target = ns.args[0] || "";
     ns.tprintf(`INFO killing script: ${target}`)
     if (typeof target === 'string') {
     
-        await walk(ns,"home",async (ns,host,target): Promise<boolean> => {
+        getAllServers(ns).forEach( host => {
             ns.ps(host).filter(x => target ==="" ||x.filename.indexOf(target)>-1).forEach(x =>{
             ns.kill(x.pid)});
-
-            return true;
-        },target)
+        })
         ns.tprintf(`INFO: done killing all instances of ${target}`)
     }
 }
--- src/net/findLit.ts
+++ src/net/findLit.ts
@@ -1,14 +1,13 @@
 import { NS } from '@ns'
-import { walk } from '/utils/utils'
+import { getAllServers } from '/utils/utils'
 
 export async function main(ns: NS): Promise<void> {
-    await walk(ns, "home", async (ns, host): Promise<boolean> => {
+    getAllServers(ns).forEach(async (host) => {
         if (typeof host === 'string') {
             const lit = ns.ls(host, ".lit")
             if (lit.length > 0) {
                 await ns.scp(lit, host, "home")
             }
         }
-        return true;
     });
 }
