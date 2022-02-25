e639c975c32a0e24c774f85d6ac94828460a9c40
James Tanner <jamesptanner@outlook.com>
2022-02-25 07:51:43 +0000

    Let player know to backdoor a server explicity.

--- src/net/walker.ts
+++ src/net/walker.ts
@@ -1,33 +1,36 @@
 import { NS } from '@ns'
 import { HGWPath } from "/utils/HGW";
 import { utilsPath, getAllServers } from "/utils/utils";
-import { filesPath, hackHostPath, infiltratePath,findNewTargetPath,hackHostLitePath } from "/hosts/files";
+import { filesPath, hackHostPath, infiltratePath, findNewTargetPath, hackHostLitePath } from "/hosts/files";
 
 export async function main(ns: NS): Promise<void> {
     const toBackdoor: string[] = []
     const servers = getAllServers(ns)
-    const ignoreHosts:string[] = JSON.parse(ns.read("ignoreHosts.txt")||"[]");
-    const preferredTarget:string =ns.read("target.txt")||""
+    const ignoreHosts: string[] = JSON.parse(ns.read("ignoreHosts.txt") || "[]");
+    const preferredTarget: string = ns.read("target.txt") || ""
 
-    if(preferredTarget){
+    if (preferredTarget) {
         ns.tprintf(`INFO coordinating attack on ${preferredTarget}`)
     }
 
-    for(const server of servers.filter(x => ignoreHosts.indexOf(x)==-1))
-    {
+    for (const server of servers.filter(x => ignoreHosts.indexOf(x) == -1)) {
         const target = preferredTarget || server
-        // ns.tprintf(`INFO ${server} attacking ${target}`)
 
         const serverInfo = ns.getServer(server);
         if (!serverInfo.backdoorInstalled) {
-            const targetHackLevel = ns.getServerRequiredHackingLevel(server);
-            if (targetHackLevel <= ns.getHackingLevel()) {
-                ns.tprintf(`INFO 💣 ${server}`);
+            if (!serverInfo.hasAdminRights) {
+                const targetHackLevel = ns.getServerRequiredHackingLevel(server);
+                if (targetHackLevel <= ns.getHackingLevel()) {
+                    ns.tprintf(`INFO 💣 ${server}`);
 
-                if (!serverInfo.purchasedByPlayer) {
-                    ns.exec(infiltratePath, "home", 1, server);
-                    toBackdoor.push(server);
+                    if (!serverInfo.purchasedByPlayer) {
+                        ns.exec(infiltratePath, "home", 1, server);
+                        toBackdoor.push(server);
+                    }
                 }
+            }
+            else {
+                ns.tprintf(`WARN 💻 Backdoor ${server}`);
             }
         }
         else if (serverInfo.backdoorInstalled && server && !scriptIsRunning(ns, server, hackHostPath) && !scriptIsRunning(ns, server, hackHostLitePath) && serverInfo.maxRam != 0) {
@@ -35,7 +38,7 @@ export async function main(ns: NS): Promise<void> {
             const availableRam = serverInfo.maxRam - serverInfo.ramUsed;
             ns.print(`Mem: available:${availableRam}, total:${serverInfo.maxRam}, needed:${memReq} threads=${Math.floor(availableRam / memReq)}`);
             if (Math.floor(availableRam / memReq) != 0) {
-                await ns.scp([HGWPath, hackHostPath, utilsPath,filesPath,findNewTargetPath], target);
+                await ns.scp([HGWPath, hackHostPath, utilsPath, filesPath, findNewTargetPath], target);
                 if (ns.exec(hackHostPath, server, Math.floor(availableRam / memReq), target) == 0) {
                     ns.tprintf(`failed to launch script on ${server}`);
                 }
@@ -46,15 +49,15 @@ export async function main(ns: NS): Promise<void> {
                 const liteMemReq = ns.getScriptRam(hackHostLitePath);
                 ns.print(`Mem: available:${availableRam}, total:${serverInfo.maxRam}, needed:${liteMemReq} threads=${Math.floor(availableRam / liteMemReq)}`);
                 if (Math.floor(availableRam / liteMemReq) != 0) {
-                    await ns.scp([HGWPath, hackHostLitePath, utilsPath,filesPath], server);
+                    await ns.scp([HGWPath, hackHostLitePath, utilsPath, filesPath], server);
                     if (ns.exec(hackHostLitePath, server, Math.floor(availableRam / liteMemReq), target) == 0) {
                         ns.tprintf(`failed to launch script on ${server}`);
                     }
                 }
-                else{
-                    ns.tprintf(`ERROR Unable to run either hack script.`) 
+                else {
+                    ns.tprintf(`ERROR Unable to run either hack script.`)
                     ignoreHosts.push(server)
-                    await ns.write("ignoreHosts.txt",JSON.stringify(ignoreHosts),"w")
+                    await ns.write("ignoreHosts.txt", JSON.stringify(ignoreHosts), "w")
                 }
             }
         }
