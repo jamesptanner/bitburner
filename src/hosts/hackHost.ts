5990aa54593ed510978dc723edb1f356eb9db75a
James Tanner <jamesptanner@outlook.com>
2022-02-27 23:14:46 +0000

    forgot to loop.

--- src/hosts/hackHost.ts
+++ src/hosts/hackHost.ts
@@ -5,22 +5,23 @@ export async function main(ns: NS): Promise<void> {
     const target = ns.args[0];
     ns.tprintf(`INFO hacking target: ${target}`);
     if (typeof target === 'string') {
-
-        const current = ns.getServerMoneyAvailable(target)
-        const max = ns.getServerMaxMoney(target)
-        const percent = current / max;
-        // ns.tprintf(`${target} money, curr:${current} max:${max} ${percent}%%`)
-        if (!(ns.getServerSecurityLevel(target) < ns.getServerMinSecurityLevel(target) + 1)) {
-            ns.print(`INFO 😷: ${target}. ${(ns.getWeakenTime(target) / 1000).toFixed(2)}s`)
-            await weakenServer(ns, target);
-        }
-        else if (percent < 1) {
-            ns.print(`INFO 🎈: ${target}. ${(ns.getGrowTime(target) / 1000).toFixed(2)}s`)
-            await growServer(ns, target);
-        }
-        else {
-            ns.print(`INFO 🤖: ${target}. ${(ns.getHackTime(target) / 1000).toFixed(2)}s`)
-            await attack(ns, target);
+        while (true) {
+            const current = ns.getServerMoneyAvailable(target)
+            const max = ns.getServerMaxMoney(target)
+            const percent = current / max;
+            // ns.tprintf(`${target} money, curr:${current} max:${max} ${percent}%%`)
+            if (!(ns.getServerSecurityLevel(target) < ns.getServerMinSecurityLevel(target) + 1)) {
+                ns.print(`INFO 😷: ${target}. ${(ns.getWeakenTime(target) / 1000).toFixed(2)}s`)
+                await weakenServer(ns, target);
+            }
+            else if (percent < 1) {
+                ns.print(`INFO 🎈: ${target}. ${(ns.getGrowTime(target) / 1000).toFixed(2)}s`)
+                await growServer(ns, target);
+            }
+            else {
+                ns.print(`INFO 🤖: ${target}. ${(ns.getHackTime(target) / 1000).toFixed(2)}s`)
+                await attack(ns, target);
+            }
         }
     }
 }
