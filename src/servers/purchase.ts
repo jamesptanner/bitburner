b93f67c60b1c9541baa824cbcf004ff8b2f617d3
James Tanner <jamesptanner@outlook.com>
2022-03-08 21:17:38 +0000

    purchase a server.

---
+++ src/servers/purchase.ts
@@ -0,0 +1,17 @@
+import { NS } from '@ns'
+
+export const purchasePath = "/servers/purchase.js";
+
+export async function main(ns: NS): Promise<void> {
+    const [name, level] = ns.args
+    if (typeof name == 'string' && typeof level == 'number') {
+        const size = 2 << level;
+        const newHost = ns.purchaseServer(name, size)
+        if (newHost === "") {
+            ns.toast("Failed to purchase server", "error")
+        }
+        else {
+            ns.toast(`purchased server ${newHost} size: ${size}GB`)
+        }
+    }
+}
