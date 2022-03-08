fa1ccc7ff589a5a0d5b2963d91b94d9c06f0fcd9
James Tanner <jamesptanner@outlook.com>
2022-03-08 21:16:48 +0000

    infiltrate from file if no host past

--- src/hosts/infiltrate.ts
+++ src/hosts/infiltrate.ts
@@ -1,46 +1,59 @@
 import { NS } from '@ns'
 import { getNumberOfTools, hasFTP, hasHTTP, hasSSH, hasSMTP, hasSQL } from '/shared/HGW';
 
-export const infiltratePath ="/hosts/infiltrate.js";
+export const infiltratePath = "/hosts/infiltrate.js";
+
+const infiltrate = function (ns:NS, host:string) {
+    const targetHackLevel = ns.getServerRequiredHackingLevel(host)
+            if (targetHackLevel > ns.getHackingLevel()) {
+                ns.tprintf(`WARN not able to hack host: ${host}(${targetHackLevel})`)
+                return
+            }
+            const server = ns.getServer(host)
+            if (server.openPortCount < server.numOpenPortsRequired) {
+                if (server.numOpenPortsRequired <= getNumberOfTools(ns)) {
+                    if (!server.ftpPortOpen && hasFTP(ns)) {
+                        ns.ftpcrack(host)
+                    }
+                    if (!server.httpPortOpen && hasHTTP(ns)) {
+                        ns.httpworm(host)
+                    }
+                    if (!server.sshPortOpen && hasSSH(ns)) {
+                        ns.brutessh(host)
+                    }
+                    if (!server.smtpPortOpen && hasSMTP(ns)) {
+                        ns.relaysmtp(host)
+                    }
+                    if (!server.sqlPortOpen && hasSQL(ns)) {
+                        ns.sqlinject(host)
+                    }
+                }
+                else {
+                    ns.tprintf(`WARN not enough tools to hack host: ${host}`)
+                    return;
+                }
+            }
+            ns.tprintf(`INFO nuking host: ${host}`)
+            ns.nuke(host);
+            ns.tprintf(`INFO installing backdoor: ${host}`)
+            //await ns.installBackdoor()
+            ns.tprintf(`INFO host ready: ${host}`)
+}
+
 
 export async function main(ns: NS): Promise<void> {
-    const target = ns.args[0];
-    ns.tprintf(`INFO infiltrating target: ${target}`)
-    if (typeof target === 'string') {
-        const targetHackLevel = ns.getServerRequiredHackingLevel(target)
-        if (targetHackLevel > ns.getHackingLevel()) {
-            ns.tprintf(`WARN not able to hack host: ${target}(${targetHackLevel})`)
-            return
+    if (ns.args.length == 1) {
+        const target = ns.args[0];
+        ns.tprintf(`INFO infiltrating target: ${target}`)
+        if (typeof target === 'string') {
+            infiltrate(ns,target)
         }
-        const server = ns.getServer(target)
-        if (server.openPortCount < server.numOpenPortsRequired) {
-            if (server.numOpenPortsRequired <= getNumberOfTools(ns)) {
-                if (!server.ftpPortOpen && hasFTP(ns)) {
-                    ns.ftpcrack(target)
-                }
-                if (!server.httpPortOpen && hasHTTP(ns)) {
-                    ns.httpworm(target)
-                }
-                if (!server.sshPortOpen && hasSSH(ns)) {
-                    ns.brutessh(target)
-                }
-                if (!server.smtpPortOpen && hasSMTP(ns)) {
-                    ns.relaysmtp(target)
-                }
-                if (!server.sqlPortOpen && hasSQL(ns)) {
-                    ns.sqlinject(target)
-                }
-            }
-            else {
-                ns.tprintf(`WARN not enough tools to hack host: ${target}`)
-                return;
-            }
-        }
-        ns.tprintf(`INFO nuking host: ${target}`)
-        ns.nuke(target);
-        ns.tprintf(`INFO installing backdoor: ${target}`)
-        //await ns.installBackdoor()
-        ns.tprintf(`INFO host ready: ${target}`)
-
+    }
+    else {
+        const targets: Array<string> = JSON.parse(ns.read("toInfiltrate.txt"))
+        targets.forEach(async target => {
+            ns.tprintf(`INFO infiltrating target: ${target}`)
+            infiltrate(ns,target)
+        })
     }
 }
