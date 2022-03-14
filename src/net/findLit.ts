145fcdba927b8314eddbdd7459af9981ee722eb4
James Tanner <jamesptanner@outlook.com>
2022-03-14 16:08:59 +0000

    change to handle async loop.

--- src/net/findLit.ts
+++ src/net/findLit.ts
@@ -2,10 +2,12 @@ import { NS } from '@ns'
 import { getAllServers } from '/shared/utils'
 
 export async function main(ns: NS): Promise<void> {
-    getAllServers(ns).forEach(async (host) => {
-            const lit = ns.ls(host, ".lit")
-            if (lit.length > 0) {
-                await ns.scp(lit, host, "home")
-            }
-    });
+    const servers = getAllServers(ns)
+
+    for (const host of servers){
+        const lit = ns.ls(host, ".lit")
+        if (lit.length > 0) {
+            await ns.scp(lit, host, "home")
+        }
+    }
 }
