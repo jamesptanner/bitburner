7bac4e3ece1580111afa2ddcdd0cbbc8631534e3
James Tanner <jamesptanner@outlook.com>
2022-03-30 01:06:14 +0100

    Create bootstrap basics for release process.

---
+++ createDistMap.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const glob = require('glob')
+
+const sourceDir = 'dist/public'
+const packageDir = 'dist/package'
+
+fs.mkdir('dist/package',function(){})
+
+const fileMap = {}
+glob.sync(`${sourceDir}/**/*.js`).forEach(path =>{
+    if(path !== `${sourceDir}/bootstrap.js`){
+        const genName = path.replace(sourceDir,'').replaceAll("/","~",)
+        fileMap[path.replace(sourceDir,'')] = genName
+        fs.copyFile(path,`${packageDir}/${genName}`,function(){})
+    }
+    else{
+        fs.copyFile(path,`${packageDir}/bootstrap.js`,function(){})
+
+    }
+})
+fs.writeFileSync(`${packageDir}/map.txt`,JSON.stringify(fileMap))
