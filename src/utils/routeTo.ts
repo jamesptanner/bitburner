7222803b148256b28a6b2a63178615f216f45c96
James Tanner <jamesptanner@outlook.com>
2022-04-15 17:03:44 +0100

    add function to route to any machine from home.

---
+++ src/utils/routeTo.ts
@@ -0,0 +1,16 @@
+import { NS } from '@ns'
+import { initLogging } from '/shared/logging';
+import { routeToHost } from '/shared/utils';
+
+export const routeToPath ="/utils/routeTo.js";
+
+export async function main(ns : NS) : Promise<void> {
+    await initLogging(ns)
+
+    const opts  = ns.flags([["host","home"]])
+    const hops = routeToHost(ns, ns.singularity.getCurrentServer(), opts.host)
+    if (hops && hops.length > 0) {
+        ns.tprintf(`routing via ${hops}`)
+        hops.forEach(hop => ns.singularity.connect(hop))
+    }
+}
