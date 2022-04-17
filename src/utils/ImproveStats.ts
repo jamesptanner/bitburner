92330520153b2c99e54a57331d4f5fed44e13590
James Tanner <jamesptanner@outlook.com>
2022-04-17 22:35:21 +0100

    feat: improve all stats at once.

--- src/utils/ImproveStats.ts
+++ src/utils/ImproveStats.ts
@@ -8,9 +8,10 @@ export const improveCombatStatsPath ="/utils/improveCombatStats.js";
 export async function main(ns : NS) : Promise<void> {
     await initLogging(ns)
     ns.disableLog('ALL')
-    const args = ns.flags([["combat",0],["charisma",0],["hacking",0]])
+    const args = ns.flags([["combat",0],["charisma",0],["hacking",0],["all",0]])
     const hacking = args.hacking
     const charisma = args.charisma
     const combat = args.combat
-   await improveStat(ns, hacking,combat,charisma)
+    const all = args.all
+   await improveStat(ns, Math.max(all,hacking),Math.max(all,combat),Math.max(all,charisma))
 }
