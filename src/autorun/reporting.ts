a15053c8b678146e202a0f726e3888cd5962c6dc
James Tanner <jamesptanner@outlook.com>
2022-04-17 22:28:38 +0100

    feat: add more metrics to reporting bug: remove other invalid file characters.

--- src/autorun/reporting.ts
+++ src/autorun/reporting.ts
@@ -1,4 +1,5 @@
 import { NS } from '@ns'
+import { moveSyntheticComments } from 'typescript';
 import { initLogging, sendMetric,log, Level } from '/shared/logging';
 import { getAllServers } from '/shared/utils';
 
@@ -6,6 +7,43 @@ export const reportingPath ="/autorun/reporting.js";
 
 export async function main(ns : NS) : Promise<void> {
     await initLogging(ns)
+    const constPlayer = ns.getPlayer()
+    // if(constPlayer.bitNodeN !=5 || ns.getOwnedSourceFiles()[5].>){
+    //     const bitnodeMultiplier = ns.getBitNodeMultipliers()
+    // }
+
+    sendMetric("player.multiplier.hacking.chance",constPlayer.hacking_chance_mult)
+    sendMetric("player.multiplier.hacking.speed",constPlayer.hacking_speed_mult)
+    sendMetric("player.multiplier.hacking.money",constPlayer.hacking_money_mult)
+    sendMetric("player.multiplier.hacking.growth",constPlayer.hacking_grow_mult)
+
+    sendMetric("player.multiplier.stat.hacking.level",constPlayer.hacking_mult)
+    sendMetric("player.multiplier.stat.hacking.exp",constPlayer.hacking_exp_mult)
+    sendMetric("player.multiplier.stat.strength.level",constPlayer.strength_mult)
+    sendMetric("player.multiplier.stat.strength.exp",constPlayer.strength_exp_mult)
+    sendMetric("player.multiplier.stat.defense.level",constPlayer.defense_mult)
+    sendMetric("player.multiplier.stat.defense.exp",constPlayer.defense_exp_mult)
+    sendMetric("player.multiplier.stat.dexterity.level",constPlayer.dexterity_mult)
+    sendMetric("player.multiplier.stat.dexterity.exp",constPlayer.dexterity_exp_mult)
+    sendMetric("player.multiplier.stat.agility.level",constPlayer.agility_mult)
+    sendMetric("player.multiplier.stat.agility.exp",constPlayer.agility_exp_mult)
+    sendMetric("player.multiplier.stat.charisma.level",constPlayer.charisma_mult)
+    sendMetric("player.multiplier.stat.charisma.exp",constPlayer.charisma_exp_mult)
+
+    sendMetric("player.multiplier.hacknet.node.production",constPlayer.hacknet_node_money_mult)
+    sendMetric("player.multiplier.hacknet.node.purchase_cost",constPlayer.hacknet_node_purchase_cost_mult)
+    sendMetric("player.multiplier.hacknet.node.ram_upgrade_cost",constPlayer.hacknet_node_ram_cost_mult)
+    sendMetric("player.multiplier.hacknet.node.core_upgrade_cost",constPlayer.hacknet_node_core_cost_mult)
+    sendMetric("player.multiplier.hacknet.node.level_upgrade_cost",constPlayer.hacknet_node_level_cost_mult)
+
+    sendMetric("player.multiplier.reputation.faction_gain",constPlayer.faction_rep_mult)
+    sendMetric("player.multiplier.reputation.company_gain",constPlayer.company_rep_mult)
+    sendMetric("player.multiplier.reputation.salary",constPlayer.work_money_mult)
+    sendMetric("player.multiplier.hacknet.node.level_upgrade_cost",constPlayer.hacknet_node_level_cost_mult)
+
+    sendMetric("player.multiplier.crime.success",constPlayer.crime_success_mult)
+    sendMetric("player.multiplier.crime.money",constPlayer.crime_money_mult)
+
     while(true){
         const player = ns.getPlayer()
         sendMetric("player.money",player.money)
