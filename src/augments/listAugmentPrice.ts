454e80f83e695072244501b12a801d0e2c6e62f1
James Tanner <jamesptanner@outlook.com>
2022-04-19 22:04:40 +0100

    feat: new table function. list augments in table.

--- src/augments/listAugmentPrice.ts
+++ src/augments/listAugmentPrice.ts
@@ -2,21 +2,22 @@ import { NS } from '@ns'
 import { initLogging, Level, log } from '/shared/logging';
 import { factions } from 'shared/factions';
 import { getAugmentsAvailableFromFaction } from '../shared/factions';
+import { makeTable } from '/shared/ui';
 
 export const listAugmentsPath = "/utils/listAugments.js";
 
 export async function main(ns: NS): Promise<void> {
     await initLogging(ns)
+    ns.clearLog()
     log(Level.Info, `Getting list  of augments`)
     factions.forEach(faction => {
         const augments = getAugmentsAvailableFromFaction(ns, faction)
         if (augments.length > 0) {
-            log(Level.Info, `faction: ${faction}, augments: [`)
-            augments.forEach(augment => {
-
-                log(Level.Info, `${augment}:${ns.nFormat(ns.singularity.getAugmentationPrice(augment), '($0.00a)')}`)
-            })
-            log(Level.Info, `]\n`)
+            const headers= ['augment','price']
+            const data = augments.map(aug=>{return [aug,ns.nFormat(ns.singularity.getAugmentationPrice(aug), '($0.00a)')]})
+            ns.print(faction)
+            makeTable(ns,headers,data,1)
+            ns.print("")
         }
     })
 }
---
+++ src/shared/ui.ts
@@ -0,0 +1,39 @@
+import { NS } from "/../NetscriptDefinitions"
+
+export const makeTable = function(ns:NS, headers: string[], data: string[][],padding = 1): void {
+    const getLineLength = function(minColWidths: number[],padding:number):number{
+        //text length + padding each side of text + len(entries)+ seperators
+        return minColWidths.reduce((p,n) =>{return p + n + 2*padding}) + minColWidths.length+3
+    }
+    const getMinColWidth = function(rows: string[],padding:number):number{
+        return rows.map(row =>{return row.length}).reduce((p,n)=>{
+            return Math.max(p,n+(padding*2))
+        })
+    }
+    const makeRowSplit = function(length:number):string{
+        return '-'.repeat(length)+'\n'
+    }
+
+    const padCell = function(content:string, width:number):string{
+        const paddingCells = width-content.length
+        return `${' '.repeat(Math.floor(paddingCells/2))}${content}${' '.repeat(Math.ceil(paddingCells/2))}`
+    }
+
+    const makeRow = function(values:string[],widths:number[], padding:number):string{
+        const paddedCells = values.map((v,i) =>{return `${' '.repeat(padding)}${padCell(v,widths[i])}${' '.repeat(padding)}`})
+        return `|${paddedCells.join('|')}|\n`
+    }
+
+    const extractColumnValues = function(data:string[][],column:number):string[]{
+        return data.map(r =>{ return r[column]})
+    }
+
+    const widths = headers.map((v,i)=>{ return getMinColWidth([v,...extractColumnValues(data,i)],padding)})
+    const lineLength = getLineLength(widths,padding)
+
+    const headerRow = makeRow(headers,widths,padding)
+    const seperator = makeRowSplit(lineLength)
+    const dataRows = data.map(d =>{return makeRow(d,widths,padding)})
+    const joinedRows = dataRows.join(`${seperator}`)
+    ns.printf(`${seperator}${headerRow}${seperator}${joinedRows}${seperator}`)
+}
