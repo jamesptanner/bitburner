e67e674add57238f908d07d0a87e6de8686534b1
James Tanner <jamesptanner@outlook.com>
2022-04-21 18:16:24 +0100

    Start monitoring the stock market to get some idea how it behaves

---
+++ src/stockMarket/monitorMarket.ts
@@ -0,0 +1,21 @@
+import { NS } from '@ns'
+import { initLogging, sendMetric } from '/shared/logging';
+
+export const monitorMarketPath ="/stockMarket/monitorMarket.js";
+
+export async function main(ns : NS) : Promise<void> {
+    await initLogging(ns)
+    const symbols = ns.stock.getSymbols()
+    
+    while(true){
+
+        symbols.forEach(sym=>{
+            sendMetric(`stock.${sym}.price`,ns.stock.getPrice(sym))
+            sendMetric(`stock.${sym}.askprice`,ns.stock.getAskPrice(sym))
+            sendMetric(`stock.${sym}.bidprice`,ns.stock.getBidPrice(sym))
+            sendMetric(`stock.${sym}.forcast`,ns.stock.getForecast(sym))
+            sendMetric(`stock.${sym}.volatility`,ns.stock.getVolatility(sym))
+        })
+        await ns.sleep(24000)
+    }
+}
