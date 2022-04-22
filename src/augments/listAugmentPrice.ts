26ba63da3b159e257c36d538735f56a0bb1acd85
James Tanner <jamesptanner@outlook.com>
2022-04-22 10:12:27 +0100

    tidy up augment price list a bit.

--- src/augments/listAugmentPrice.ts
+++ src/augments/listAugmentPrice.ts
@@ -9,15 +9,18 @@ export const listAugmentsPath = "/utils/listAugments.js";
 export async function main(ns: NS): Promise<void> {
     await initLogging(ns)
     ns.clearLog()
-    log(Level.Info, `Getting list  of augments`)
+    log(Level.Info, `Getting list of augments`)
+
+    const playerInFaction = (faction:string):boolean =>{
+        return ns.getPlayer().factions.indexOf(faction) !== -1
+    }
     factions.forEach(faction => {
         const augments = getAugmentsAvailableFromFaction(ns, faction)
         if (augments.length > 0) {
-            const headers= ['augment','price']
-            const data = augments.map(aug=>{return [aug,ns.nFormat(ns.singularity.getAugmentationPrice(aug), '($0.00a)')]})
-            ns.print(faction)
+            const headers= ['augment','reputation','price']
+            const data = augments.map(aug=>{return [aug,ns.nFormat(ns.singularity.getAugmentationRepReq(aug), '(0.000a)'),ns.nFormat(ns.singularity.getAugmentationPrice(aug), '($0.00a)')]})
+            ns.print(`${faction}: ${playerInFaction(faction)? ns.nFormat(ns.singularity.getFactionRep(faction), '(0.000a)') : "Locked"}`)
             makeTable(ns,headers,data,1)
-            ns.print("")
         }
     })
 }
