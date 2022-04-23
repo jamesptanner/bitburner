b4047a76a3e10c273f47336b4d9c9e0c72043879
James Tanner <jamesptanner@outlook.com>
2022-04-23 08:43:27 +0100

    only send s4 metrics if we have the s4 api. send error if we have no api.

--- src/stockMarket/monitorMarket.ts
+++ src/stockMarket/monitorMarket.ts
@@ -1,20 +1,27 @@
 import { NS } from '@ns'
 import { initLogging, sendMetric } from '/shared/logging';
+import { error } from './../shared/logging';
 
-export const monitorMarketPath ="/stockMarket/monitorMarket.js";
+export const monitorMarketPath = "/stockMarket/monitorMarket.js";
 
-export async function main(ns : NS) : Promise<void> {
+export async function main(ns: NS): Promise<void> {
     await initLogging(ns)
     const symbols = ns.stock.getSymbols()
-    
-    while(true){
+    const has4s = ns.getPlayer().has4SDataTixApi
 
-        symbols.forEach(sym=>{
-            sendMetric(`stock.${sym}.price`,ns.stock.getPrice(sym))
-            sendMetric(`stock.${sym}.askprice`,ns.stock.getAskPrice(sym))
-            sendMetric(`stock.${sym}.bidprice`,ns.stock.getBidPrice(sym))
-            sendMetric(`stock.${sym}.forcast`,ns.stock.getForecast(sym))
-            sendMetric(`stock.${sym}.volatility`,ns.stock.getVolatility(sym))
+    if(!ns.getPlayer().hasTixApiAccess){
+        error("Dont have TIX API access",true)
+    }
+    while (true) {
+
+        symbols.forEach(sym => {
+            sendMetric(`stock.${sym}.price`, ns.stock.getPrice(sym))
+            sendMetric(`stock.${sym}.askprice`, ns.stock.getAskPrice(sym))
+            sendMetric(`stock.${sym}.bidprice`, ns.stock.getBidPrice(sym))
+            if (has4s) {
+                sendMetric(`stock.${sym}.forcast`, ns.stock.getForecast(sym))
+                sendMetric(`stock.${sym}.volatility`, ns.stock.getVolatility(sym))
+            }
         })
         await ns.sleep(48000)
     }
