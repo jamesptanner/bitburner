56c5209d0eec8b2ffd33ef01860d0f831bab33ad
James Tanner <jamesptanner@outlook.com>
2022-04-27 22:19:17 +0100

    properly calculate money needed for next mem upgrade.

--- src/playmode/lowMem.ts
+++ src/playmode/lowMem.ts
@@ -8,8 +8,11 @@ export async function main(ns: NS): Promise<void> {
     //not using usual logging until we have more memory.
     // await initLogging(ns) 
     while (ns.getServerMaxRam('home') <= 32) {
-        await runHacknet(ns, () => { return ns.getPlayer().money < 10.1e6 })
+        await runHacknet(ns, () => { 
+            return ns.getPlayer().money < ns.getServerMaxRam('home') * 3.2e4 * Math.pow(1.58,Math.log2(ns.getServerMaxRam('home'))) 
+        })
         ns.tprint(`Ready to buy at least 32gb memory`)
         await ns.sleep(15*60*1000)
     }
 }
+
