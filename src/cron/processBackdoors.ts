ab2839270306a3166b58ae841c820695afbc10aa
James Tanner <jamesptanner@outlook.com>
2022-05-10 23:39:52 +0100

    catch error if we dont have the singularity.

--- src/cron/processBackdoors.ts
+++ src/cron/processBackdoors.ts
@@ -1,6 +1,6 @@
 import { NS } from '@ns';
 import { routeToHost } from '/shared/utils';
-import { initLogging,logging } from '/shared/logging';
+import { initLogging, logging } from '/shared/logging';
 
 export const processBackdoorsPath = "/cron/processBackdoors.js";
 
@@ -10,19 +10,32 @@ export async function main(ns: NS): Promise<void> {
     if (hosts.length > 0) {
         logging.info(`need to backdoor : ${hosts.join()}`)
         for (const host of hosts) {
-            ns.singularity.connect("home")
-            logging.info(`backdooring ${host} starting at home}`)
+            logging.info(`backdooring ${host} starting at home`)
             const hops = routeToHost(ns, 'home', host)
             if (hops && hops.length > 0) {
-                logging.info(`routing via ${hops}`)
-
-                hops.forEach(hop => ns.singularity.connect(hop))
-                logging.info(`installing backdoor ${host}`)
-                await ns.singularity.installBackdoor();
+                try {
+                    logging.info(`routing via ${hops}`)
+                    ns.singularity.connect("home")
+                    hops.forEach(hop => ns.singularity.connect(hop))
+                    logging.info(`installing backdoor ${host}`)
+                    await ns.singularity.installBackdoor();
+                }
+                catch (e) {
+                    if (e instanceof Error) {
+                        logging.error(e.message)
+                    }
+                }
             }
         }
-        logging.info(`returning home`)
-        ns.singularity.connect("home")
-        ns.rm("toBackdoor.txt","home");
+        try {
+            logging.info(`returning home`)
+            ns.singularity.connect("home")
+            ns.rm("toBackdoor.txt", "home");
+        }
+        catch (e) {
+            if (e instanceof Error) {
+                logging.error(e.message)
+            }
+        }
     }
 }
