fe9d1cd40ec024af972c2d374e801b459a65c426
James Tanner <jamesptanner@gmail.com>
2024-03-27 22:39:28 +0000

    remove loop now cron does the report triggering.

--- src/cron/reporting.ts
+++ src/cron/reporting.ts
@@ -49,41 +49,39 @@ export async function main(ns: NS): Promise<void> {
 
     sendMetric("player.multiplier.crime.success", constPlayer.crime_success || -1)
     sendMetric("player.multiplier.crime.money", constPlayer.crime_money || -1)
+ 
+    const player = ns.getPlayer()
+    sendMetric("player.money", player.money)
+    sendMetric("player.stats.level.hack", player.skills.hacking)
+    sendMetric("player.stats.level.strength", player.skills.strength)
+    sendMetric("player.stats.level.defense", player.skills.defense)
+    sendMetric("player.stats.level.dexterity", player.skills.dexterity)
+    sendMetric("player.stats.level.agility", player.skills.agility)
+    sendMetric("player.stats.level.charisma", player.skills.charisma)
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore property is intentionally undocumented.
+    sendMetric("player.stats.level.karma", ns.heart.break())
+    sendMetric("player.bitnode", getBitnode(ns))
 
-    while (true) {
-        const player = ns.getPlayer()
-        sendMetric("player.money", player.money)
-        sendMetric("player.stats.level.hack", player.skills.hacking)
-        sendMetric("player.stats.level.strength", player.skills.strength)
-        sendMetric("player.stats.level.defense", player.skills.defense)
-        sendMetric("player.stats.level.dexterity", player.skills.dexterity)
-        sendMetric("player.stats.level.agility", player.skills.agility)
-        sendMetric("player.stats.level.charisma", player.skills.charisma)
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore property is intentionally undocumented.
-        sendMetric("player.stats.level.karma", ns.heart.break())
-        sendMetric("player.bitnode", getBitnode(ns))
-
-        getAllServers(ns).concat('home').filter(server => {
-            const serverInfo = ns.getServer(server)
-            return serverInfo.backdoorInstalled || serverInfo.purchasedByPlayer
+    getAllServers(ns).concat('home').filter(server => {
+        const serverInfo = ns.getServer(server)
+        return serverInfo.backdoorInstalled || serverInfo.purchasedByPlayer
+    })
+        .forEach(server => {
+            log(Level.Info, `server:${server} ramused:${ns.getServerUsedRam(server)} rammax:${ns.getServerMaxRam(server)}`)
         })
-            .forEach(server => {
-                log(Level.Info, `server:${server} ramused:${ns.getServerUsedRam(server)} rammax:${ns.getServerMaxRam(server)}`)
-            })
-        getAllServers(ns).concat('home')
-            .forEach(server => {
-                const ServerInfo = ns.getServer(server)
-                sendMetric(`server.${server.replaceAll(".", "-")}.hackstate`, getHackState(ns, server))
-                sendMetric(`server.${server.replaceAll(".", "-")}.playerOwned`, ServerInfo.purchasedByPlayer ? 1 : 0)
-                sendMetric(`server.${server.replaceAll(".", "-")}.maxRam`, ns.getServerMaxRam(server))
-                sendMetric(`server.${server.replaceAll(".", "-")}.usedRam`, ns.getServerUsedRam(server))
-                sendMetric(`server.${server.replaceAll(".", "-")}.securitylevel`, ns.getServerSecurityLevel(server))
-                sendMetric(`server.${server.replaceAll(".", "-")}.minsecuritylevel`, ns.getServerMinSecurityLevel(server))
-                sendMetric(`server.${server.replaceAll(".", "-")}.money`, ns.getServerMoneyAvailable(server))
-                sendMetric(`server.${server.replaceAll(".", "-")}.maxmoney`, ns.getServerMaxMoney(server))
-            })
-    }
+    getAllServers(ns).concat('home')
+        .forEach(server => {
+            const ServerInfo = ns.getServer(server)
+            sendMetric(`server.${server.replaceAll(".", "-")}.hackstate`, getHackState(ns, server))
+            sendMetric(`server.${server.replaceAll(".", "-")}.playerOwned`, ServerInfo.purchasedByPlayer ? 1 : 0)
+            sendMetric(`server.${server.replaceAll(".", "-")}.maxRam`, ns.getServerMaxRam(server))
+            sendMetric(`server.${server.replaceAll(".", "-")}.usedRam`, ns.getServerUsedRam(server))
+            sendMetric(`server.${server.replaceAll(".", "-")}.securitylevel`, ns.getServerSecurityLevel(server))
+            sendMetric(`server.${server.replaceAll(".", "-")}.minsecuritylevel`, ns.getServerMinSecurityLevel(server))
+            sendMetric(`server.${server.replaceAll(".", "-")}.money`, ns.getServerMoneyAvailable(server))
+            sendMetric(`server.${server.replaceAll(".", "-")}.maxmoney`, ns.getServerMaxMoney(server))
+        })
 }
 const getHackState = function (ns: NS, server: string): number {
     const ServerInfo = ns.getServer(server)
@@ -91,4 +89,4 @@ const getHackState = function (ns: NS, server: string): number {
     if (ServerInfo && (ServerInfo.backdoorInstalled || ServerInfo.purchasedByPlayer)) return 2
     if (ServerInfo && ( ServerInfo.openPortCount && ServerInfo.numOpenPortsRequired && ServerInfo.openPortCount >= ServerInfo.numOpenPortsRequired) && ( ServerInfo.requiredHackingSkill && ServerInfo.requiredHackingSkill <= ns.getPlayer().skills.hacking)) return 1;
     return 0
-}
+} 
