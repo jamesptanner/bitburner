342a814580a2d73a7c058069bf1264b78f0274b3
James Tanner <jamesptanner@gmail.com>
2024-05-12 12:20:17 +0100

    add text colours

--- src/shared/logging.ts
+++ src/shared/logging.ts
@@ -201,3 +201,32 @@ export class Logging {
     }
   }
 }
+
+export const TextColors = {
+  black: "\u001b[30m",
+  red: "\u001b[31m",
+  green: "\u001b[32m",
+  yellow: "\u001b[33m",
+  blue: "\u001b[34m",
+  magenta: "\u001b[35m",
+  cyan: "\u001b[36m",
+  white: "\u001b[37m",
+  brightBlack: "\u001b[30;1m",
+  brightRed: "\u001b[31;1m",
+  brightGreen: "\u001b[32;1m",
+  brightYellow: "\u001b[33;1m",
+  brightBlue: "\u001b[34;1m",
+  brightMagenta: "\u001b[35;1m",
+  brightCyan: "\u001b[36;1m",
+  brightWhite: "\u001b[37;1m",
+  reset: "\u001b[0m"
+}
+
+export function ansiRegex({onlyFirst = false} = {}): RegExp {
+	const pattern = [
+		'[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
+		'(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]))'
+	].join('|');
+
+	return new RegExp(pattern, onlyFirst ? undefined : 'g');
+}
