c9b65af9f25e75c15c330e06bcec2dd96ea453fa
James Tanner <jamesptanner@gmail.com>
2024-05-12 14:00:05 +0100

    remove the money aspect from the game class. allow providing a logger instance if we already have one.

--- src/lib/game.ts
+++ src/lib/game.ts
@@ -5,27 +5,9 @@ export class Game {
   readonly ns:NS
   readonly logging : Logging
 
-  constructor(ns: NS){
-    this.ns = ns;
-    this.logging = new Logging(ns);
+  constructor(ns:NS, logging?:Logging){
+    this.ns = ns
+    this.logging = logging ?? new Logging(ns);
   }
 
-  private seed: number | undefined 
-  private reinvestment: number | undefined 
-  private max :number | undefined 
-  public setScriptSeedReinvestment(seed: number, reinvestment: number, max?: number ){
-    this.seed = seed;
-    this.reinvestment =reinvestment;
-    this.max = max;
-  }
-
-  public getMyScriptIncome(){
-    const thisScript = this.ns.getRunningScript();
-    this.ns.getMoneySources
-    return (thisScript?.offlineMoneyMade ??0) + (thisScript?.onlineMoneyMade ??0);
-  }
-  public getAvailibleMoney() : number{
-    return 0;
-  }
-  
 }
